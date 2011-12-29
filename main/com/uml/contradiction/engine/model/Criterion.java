f01830760239e3e24849775d1cfd43a61e1ea7e0
RedFox <RedFox@192.168.1.10>
2011-12-29 13:45:53 +0200

    edit Criterion by Gleb

--- main/com/uml/contradiction/engine/model/Criterion.java
+++ main/com/uml/contradiction/engine/model/Criterion.java
@@ -2,17 +2,33 @@ package com.uml.contradiction.engine.model;
 import java.util.*;
 
 public class Criterion {
-
-	Collection<Quantifier> quantifiers;
-	Collection<BoundedPredicate> boundedPredicates;
+	private String name;
+	private Collection<Quantifier> quantifiers;
+	private Collection<BoundedPredicate> boundedPredicates;
 	private boolean negative;
-
-	public boolean isNegative() {
-		return this.negative;
+	public String getName() {
+		return name;
 	}
-
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Collection<Quantifier> getQuantifiers() {
+		return quantifiers;
+	}
+	public void setQuantifiers(Collection<Quantifier> quantifiers) {
+		this.quantifiers = quantifiers;
+	}
+	public Collection<BoundedPredicate> getBoundedPredicates() {
+		return boundedPredicates;
+	}
+	public void setBoundedPredicates(Collection<BoundedPredicate> boundedPredicates) {
+		this.boundedPredicates = boundedPredicates;
+	}
+	public boolean isNegative() {
+		return negative;
+	}
 	public void setNegative(boolean negative) {
 		this.negative = negative;
 	}
-
+	
 }
