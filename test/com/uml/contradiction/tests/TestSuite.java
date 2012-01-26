aff338aae78186ad27662089a8181c5882d71bda
Gleb Mikhaylenko <tas.nix@gmail.com>
2012-01-26 13:15:27 +0200

    add TestSuite

---
+++ test/com/uml/contradiction/tests/TestSuite.java
@@ -0,0 +1,17 @@
+package com.uml.contradiction.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import static org.junit.Assert.*;
+
+
+@SuiteClasses({EngineTest.class, MustExistClassCriterionTest.class})
+@RunWith(Suite.class)
+public class TestSuite {
+	public void test(){
+		assertTrue(true);
+	}
+}
