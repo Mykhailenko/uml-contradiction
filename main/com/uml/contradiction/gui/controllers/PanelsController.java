db615f9da8864b62c8a1072472da60a1487cc5c3
Gleb Mikhaylenko <tas.nix@gmail.com>
2012-01-29 15:25:07 +0200

    modify gui

--- main/com/uml/contradiction/gui/controllers/PanelsController.java
+++ main/com/uml/contradiction/gui/controllers/PanelsController.java
@@ -1,5 +1,21 @@
 package com.uml.contradiction.gui.controllers;
 
-public class PanelsController {
+import javax.swing.JPanel;
 
+import com.uml.contradiction.gui.panels.*;
+import com.uml.contradiction.gui.windows.MainWindow;
+
+public class PanelsController {
+	public static MainWindow mainWindow; 
+	public static StartPanel startPanel;
+	public static ContradictionsPanel contradictionsPanel;
+	public static DiagramsPanel diagramsPanel;
+	public static VerificationResultsPanel resultsPanel;
+	
+	public static void showPanel(JPanel panel) {
+		mainWindow.removeAll();
+		mainWindow.setContentPane(panel);
+		mainWindow.repaint();
+		
+	}
 }
--- main/com/uml/contradiction/gui/sceneries/LoadXMIScenery.java
+++ main/com/uml/contradiction/gui/sceneries/LoadXMIScenery.java
@@ -1,5 +1,7 @@
 package com.uml.contradiction.gui.sceneries;
 
 public class LoadXMIScenery {
-
+	public static void run() {
+		
+	}
 }
