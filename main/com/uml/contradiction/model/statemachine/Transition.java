3e4adf183bd347c3674507847ab86b5a602e30ec
е <@tasWIN>
2012-01-29 14:33:59 +0200

    complete branch

--- main/com/uml/contradiction/model/statemachine/Transition.java
+++ main/com/uml/contradiction/model/statemachine/Transition.java
@@ -1,18 +1,15 @@
 package com.uml.contradiction.model.statemachine;
 
+import java.util.List;
+
 import com.uml.contradiction.model.Vertex;
 
 public class Transition {
-	private String name;
 	private Vertex target;
 	private Vertex source;
 	private TransitionKind kind;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	private List<Trigger> triggers;
+	private Guard guard;
 	public Vertex getTarget() {
 		return target;
 	}
@@ -30,6 +27,18 @@ public class Transition {
 	}
 	public void setKind(TransitionKind kind) {
 		this.kind = kind;
+	}
+	public List<Trigger> getTriggers() {
+		return triggers;
+	}
+	public void setTriggers(List<Trigger> triggers) {
+		this.triggers = triggers;
+	}
+	public Guard getGuard() {
+		return guard;
+	}
+	public void setGuard(Guard guard) {
+		this.guard = guard;
 	}
 	
 }
