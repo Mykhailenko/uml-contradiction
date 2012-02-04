0091689810ea6b0670b567b42be6fafb39471bf2
е <@tasWIN>
2012-02-04 20:02:46 +0200

    edit Message

--- main/com/uml/contradiction/model/sequence/Message.java
+++ main/com/uml/contradiction/model/sequence/Message.java
@@ -1,9 +1,34 @@
 package com.uml.contradiction.model.sequence;
 public class Message implements InteractionElement {
 
-	MessageSort messageSort;
-	MessageKind messageKind;
-	Event sendEvent;
-	Event recieveEvent;
+	private MessageSort messageSort;
+	private MessageKind messageKind;
+	private Event sendEvent;
+	private Event recieveEvent;
+	public MessageSort getMessageSort() {
+		return messageSort;
+	}
+	public void setMessageSort(MessageSort messageSort) {
+		this.messageSort = messageSort;
+	}
+	public MessageKind getMessageKind() {
+		return messageKind;
+	}
+	public void setMessageKind(MessageKind messageKind) {
+		this.messageKind = messageKind;
+	}
+	public Event getSendEvent() {
+		return sendEvent;
+	}
+	public void setSendEvent(Event sendEvent) {
+		this.sendEvent = sendEvent;
+	}
+	public Event getRecieveEvent() {
+		return recieveEvent;
+	}
+	public void setRecieveEvent(Event recieveEvent) {
+		this.recieveEvent = recieveEvent;
+	}
+	
 
 }
