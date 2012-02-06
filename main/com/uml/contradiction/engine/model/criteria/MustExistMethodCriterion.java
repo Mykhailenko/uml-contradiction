18c7a1b20f39d53d180c90ba332dad23cfaedb5b
RedFox <RedFox@HSH3.hsh>
2012-02-06 16:59:29 +0200

    add MustExistMethodCriterion

--- main/com/uml/contradiction/engine/model/criteria/MustExistMethodCriterion.java
+++ main/com/uml/contradiction/engine/model/criteria/MustExistMethodCriterion.java
@@ -1,8 +1,19 @@
 package com.uml.contradiction.engine.model.criteria;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.uml.contradiction.engine.model.BoundedPredicate;
 import com.uml.contradiction.engine.model.Quantifier;
 import com.uml.contradiction.engine.model.QuantifierType;
 import com.uml.contradiction.engine.model.Variable;
+import com.uml.contradiction.engine.model.mapping.Mapping;
+import com.uml.contradiction.engine.model.mapping.MethodClassMapping;
+import com.uml.contradiction.engine.model.mapping.TargetMessageMapping;
+import com.uml.contradiction.engine.model.predicate.MessageBelongToMethod;
+import com.uml.contradiction.engine.model.rightPart.ComplexRightPart;
+import com.uml.contradiction.engine.model.rightPart.QuantifierRightPart;
+import com.uml.contradiction.engine.model.rightPart.simple.RPMessages;
 
 public class MustExistMethodCriterion extends Criterion {
 	public MustExistMethodCriterion() {
@@ -10,8 +21,25 @@ public class MustExistMethodCriterion extends Criterion {
 		Quantifier quantifier0 = new Quantifier();
 		quantifier0.setType(QuantifierType.ALL);
 		quantifier0.setBoundVariable(Variable.m);
+		quantifier0.setRightPart(new RPMessages());
+		getQuantifiers().add(quantifier0);
 		
+		Quantifier quantifier1 = new Quantifier();
+		quantifier1.setType(QuantifierType.ALONE);
+		quantifier1.setBoundVariable(Variable.o);
+		ComplexRightPart rightPart = new ComplexRightPart();
+		rightPart.setBoundVariable(Variable.m);
+		List<Mapping> nestedMappings = new LinkedList<Mapping>();
+		nestedMappings.add(new MethodClassMapping());
+		nestedMappings.add(new TargetMessageMapping());
+		rightPart.setNestedMappings(nestedMappings);
+		quantifier1.setRightPart(rightPart);
 		
+		BoundedPredicate boundedPredicate = new BoundedPredicate();
+		boundedPredicate.getBoundVariable().add(Variable.m);
+		boundedPredicate.getBoundVariable().add(Variable.o);
+		boundedPredicate.setPredicate(new MessageBelongToMethod());
+		getBoundedPredicates().add(boundedPredicate);
 	}
 	
 	
---
+++ main/com/uml/contradiction/engine/model/predicate/MessageBelongToMethod.java
@@ -0,0 +1,15 @@
+package com.uml.contradiction.engine.model.predicate;
+
+import java.util.List;
+
+import com.uml.contradiction.engine.model.predicate.exception.PredicatException;
+
+public class MessageBelongToMethod implements Predicate {
+
+	@Override
+	public boolean predict(List params) throws PredicatException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
