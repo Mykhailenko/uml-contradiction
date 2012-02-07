907e27b39431e56970d9f731de7b6baa02ad5d39
RedFox <RedFox@HSH3.hsh>
2012-02-07 15:19:34 +0200

    edit MenuBar

--- main/com/uml/contradiction/gui/menu/MenuBar.java
+++ main/com/uml/contradiction/gui/menu/MenuBar.java
@@ -10,13 +10,35 @@ public class MenuBar extends JMenuBar {
 	
 	public MenuBar() {
 		super();
-		
 		JMenu fileMenu = new JMenu("File");
+
 		JMenuItem loadXMI = new JMenuItem("Load XMI");
-		fileMenu.add(loadXMI);
 		loadXMI.addActionListener(new LoadXMIListener());
+		fileMenu.add(loadXMI);
+		
+		JMenuItem save = new JMenuItem("Save");
+		fileMenu.add(save);
+		
+		fileMenu.addSeparator();
+		
+		JMenuItem exit = new JMenuItem("Exit");
+		fileMenu.add(exit);
+		
 		this.add(fileMenu);
 		
+		JMenu view = new JMenu("View");
+		
+		this.add(view);
+		
+		JMenu help = new JMenu("Help");
+		
+		JMenuItem about = new JMenuItem("About");
+		help.add(about);
+		
+		JMenuItem helpContent = new JMenuItem("Help Content");
+		help.add(helpContent);
+		
+		this.add(help);
 	}
 	
 }
