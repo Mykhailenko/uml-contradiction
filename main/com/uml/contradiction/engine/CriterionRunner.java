89406bfea41ac96a1df088dcf9faecc0ecb16dfd
Gleb Mikhaylenko <tas.nix@gmail.com>
2012-02-09 00:28:11 +0200

    RunCriterion

---
+++ main/com/uml/contradiction/engine/CriterionRunner.java
@@ -0,0 +1,20 @@
+package com.uml.contradiction.engine;
+
+import com.uml.contradiction.engine.model.VerificationResult;
+import com.uml.contradiction.engine.model.criteria.Criterion;
+
+public class CriterionRunner extends Thread {
+	private Criterion criterion;
+	private VerificationResult result = null;
+	public CriterionRunner(Criterion criterion) {
+		this.criterion = criterion;
+	}
+	@Override
+	public void run() {
+		Engine engine = new Engine(criterion);
+		result = engine.verify();
+	}
+	public VerificationResult getResult() {
+		return result;
+	}
+}
---
+++ main/com/uml/contradiction/engine/RunCriterion.java
@@ -0,0 +1,37 @@
+package com.uml.contradiction.engine;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.uml.contradiction.engine.model.VerificationResult;
+import com.uml.contradiction.engine.model.criteria.Criterion;
+import com.uml.contradiction.engine.model.criteria.CriterionSuite;
+
+public class RunCriterion {
+	private static final Logger LOGGER = Logger.getRootLogger();
+	public List<VerificationResult> runAll(){
+		return run(CriterionSuite.getAllCriterion());
+	}
+	public List<VerificationResult> run(List<Criterion> criterions){
+		List<CriterionRunner> criterionRunners = new LinkedList<CriterionRunner>();
+		try {
+			for(Criterion criterion : criterions){
+				CriterionRunner criterionRunner = new CriterionRunner(criterion);
+				criterionRunners.add(criterionRunner);
+				criterionRunner.start();
+			}
+			for(CriterionRunner criterionRunner : criterionRunners){
+				criterionRunner.join();
+			}
+		} catch (InterruptedException e) {
+			LOGGER.error("OLOLO " + e);
+		}
+		List<VerificationResult> result = new LinkedList<VerificationResult>();
+		for(CriterionRunner criterionRunner : criterionRunners){
+			result.add(criterionRunner.getResult());
+		}
+		return result;
+	}
+}
