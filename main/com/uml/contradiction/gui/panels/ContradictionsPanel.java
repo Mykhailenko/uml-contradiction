07b69e10c04d254aa0916481ec759c211b454ab9
RedFox <RedFox@192.168.1.10>
2012-02-14 20:49:03 +0200

    lolo ubuntaaa

--- main/com/uml/contradiction/gui/panels/ContradictionsPanel.java
+++ main/com/uml/contradiction/gui/panels/ContradictionsPanel.java
@@ -4,6 +4,7 @@ package com.uml.contradiction.gui.panels;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,12 +17,14 @@ import javax.swing.JTextArea;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 
 import com.uml.contradiction.engine.model.criteria.Criterion;
 import com.uml.contradiction.engine.model.criteria.CriterionSuite;
 import com.uml.contradiction.engine.model.criteria.CriterionTypeSuite;
 import com.uml.contradiction.gui.components.CheckTreeManager;
 import com.uml.contradiction.gui.listeners.ContrTreeListener;
+import com.uml.contradiction.gui.listeners.StartCheckListener;
 import com.uml.contradiction.gui.models.DiagramForChoise;
 import com.uml.contradiction.gui.models.DisplayedCriterion;
 import com.uml.contradiction.gui.models.DisplayedCriterionType;
@@ -32,6 +35,7 @@ public class ContradictionsPanel extends JPanel{
 	private final JButton backBut = new JButton("<< Back");
 	private final JTree tree = new JTree();
 	private final JTextArea description = new JTextArea();
+	private CheckTreeManager checkTreeManager;
 	
 	public ContradictionsPanel() {
 		super();
@@ -59,18 +63,20 @@ public class ContradictionsPanel extends JPanel{
 		tree.addTreeSelectionListener(new ContrTreeListener());
 		
 		tree.setModel(model);
-		CheckTreeManager checkTreeManager = new CheckTreeManager(tree);
+		this.checkTreeManager = new CheckTreeManager(tree);
 		JScrollPane treePanel = new JScrollPane(tree);
 		JScrollPane descriptionPanel = new JScrollPane(description);
 		description.setEditable(false);
 		
-		
+		startBut.addActionListener(new StartCheckListener());
 		
 		this.setLayout(null);
 		treePanel.setBounds(0, 0, 300, 300);
 		descriptionPanel.setBounds(310, 0, 300, 300);
+		startBut.setBounds(10, 310, 100, 25);
 		this.add(treePanel);
 		this.add(descriptionPanel);
+		this.add(startBut);
 		
 		this.updateUI();
 		this.repaint();
@@ -90,6 +96,22 @@ public class ContradictionsPanel extends JPanel{
 			this.description.updateUI();
 			this.updateUI();			
 		}
+	}
+	
+	public List<Criterion> getSelectedCriterions() {
+		List<Criterion> res = new LinkedList<Criterion>();
+		
+		TreePath[] paths = checkTreeManager.getSelectionModel().getSelectionPaths(); 
+		for(TreePath path:paths) {
+			System.out.println(((DefaultMutableTreeNode)path.getLastPathComponent()).getUserObject().toString());
+			System.out.println(path.getClass());
+		}
+		
+		return res;
+	}
+	
+	public JTree getTree() {
+		return tree;
 	}
 	
 }
