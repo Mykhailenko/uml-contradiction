6bff3cf62c219729e120422046116d43b69ad646
Yaroslav Shatalov <shatalov_yarik@mail.ru>
2012-02-29 00:54:11 +0200

    Added stubs for other parsers

---
+++ main/com/uml/contradiction/converter/core/statemachine/StatemachineParser.java
@@ -0,0 +1,17 @@
+package com.uml.contradiction.converter.core.statemachine;
+
+import java.util.List;
+
+import org.w3c.dom.Element;
+
+import com.uml.contradiction.converter.core.CoreParser;
+import com.uml.contradiction.converter.core.CoreParserImpl;
+
+public class StatemachineParser 
+extends CoreParserImpl implements CoreParser{
+	
+
+	public List<Object> parse(Element umlModelEl) {
+		return null;
+	}
+}
