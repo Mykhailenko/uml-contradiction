9cd98afb1fe0284a33ed5f6a1c5455db9cacf5ce
RedFox <RedFox@HSH3.hsh>
2012-04-30 15:44:53 +0300

    ProjectDialog
    Change-Id: I7d19c8a8861bb1dd22fb17d2e7ce2cddd469ed19

--- main/com/uml/contradiction/gui/components/ProjectInfoDialog.java
+++ main/com/uml/contradiction/gui/components/ProjectInfoDialog.java
@@ -1,7 +1,14 @@
 package com.uml.contradiction.gui.components;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
 
 import com.uml.contradiction.gui.Client;
 import com.uml.contradiction.model.MetaData;
@@ -34,60 +41,81 @@ public class ProjectInfoDialog extends JDialog {
 		setTitle("Project Information");
 		
 		tprojectName = new JLabel("Project name: ");
-		tprojectName.setBounds(10, 10, 110, 20);
+		tprojectName.setBounds(10, 10, 100, 20);
+		tprojectName.setHorizontalAlignment(JLabel.RIGHT);
 		add(tprojectName);
 		
 		projectName = new JLabel(MetaData.getProjectName());
-		projectName.setBounds(120, 10, 270, 20);
+		projectName.setBounds(120, 10, 260, 20);
+		setSomeSets(projectName);
 		add(projectName);
 		
 		texporter = new JLabel("Exporter: ");
-		texporter.setBounds(10, 40, 110, 20);
+		texporter.setBounds(10, 40, 100, 20);
+		texporter.setHorizontalAlignment(JLabel.RIGHT);
 		add(texporter);
 		
 		exporter = new JLabel(MetaData.getExporter());
-		exporter.setBounds(120, 40, 270, 20);
+		exporter.setBounds(120, 40, 260, 20);
+		setSomeSets(exporter);
 		add(exporter);
 		
 		tauthor = new JLabel("Author: ");
-		tauthor.setBounds(10, 70, 110, 20);
+		tauthor.setBounds(10, 70, 100, 20);
+		tauthor.setHorizontalAlignment(JLabel.RIGHT);
 		add(tauthor);
 		
 		author = new JLabel(MetaData.getAuthor());
-		author.setBounds(120, 70, 270, 20);
+		author.setBounds(120, 70, 260, 20);
+		setSomeSets(author);
 		add(author);
 		
 		tcompany  = new JLabel("Company: ");
-		tcompany.setBounds(10, 100, 110, 20);
+		tcompany.setBounds(10, 100, 100, 20);
+		tcompany.setHorizontalAlignment(JLabel.RIGHT);
 		add(tcompany);
 		
 		company = new JLabel(MetaData.getCompany());
-		company.setBounds(120, 100, 270, 20);
+		company.setBounds(120, 100, 260, 20);
+		setSomeSets(company);
 		add(company);
 		
 		tdescription = new JLabel("Description: ");
-		tdescription.setBounds(10, 130, 110, 20);
+		tdescription.setBounds(10, 130, 100, 20);
+		tdescription.setHorizontalAlignment(JLabel.RIGHT);
 		add(tdescription);
 		
 		description = new JLabel(MetaData.getDescription());
-		description.setBounds(120, 130, 270, 20);
+		description.setBounds(120, 130, 260, 20);
+		setSomeSets(description);
 		add(description);
 		
 		tpmCreateDateTime = new JLabel("Created date: ");
-		tpmCreateDateTime.setBounds(10, 160, 110, 20);
+		tpmCreateDateTime.setBounds(10, 160, 100, 20);
+		tpmCreateDateTime.setHorizontalAlignment(JLabel.RIGHT);
 		add(tpmCreateDateTime);
 		
 		pmCreateDateTime = new JLabel(MetaData.getPmCreateDateTime());
-		pmCreateDateTime.setBounds(120, 160, 270, 20);
+		pmCreateDateTime.setBounds(120, 160, 260, 20);
+		setSomeSets(pmCreateDateTime);
 		add(pmCreateDateTime);
 		
 		tpmLastModified = new JLabel("Last modified: ");
-		tpmLastModified.setBounds(10, 190, 110, 20);
+		tpmLastModified.setBounds(10, 190, 100, 20);
+		tpmLastModified.setHorizontalAlignment(JLabel.RIGHT);
 		add(tpmLastModified);
 		
 		pmLastModified = new JLabel(MetaData.getPmLastModified());
-		pmLastModified.setBounds(120, 190, 270, 20);
+		pmLastModified.setBounds(120, 190, 260, 20);
+		setSomeSets(pmLastModified);
 		add(pmLastModified);
+	}
+	private void setSomeSets(JLabel label){
+		label.setForeground(Color.BLACK);
+		label.setBackground(Color.WHITE);
+		label.setOpaque(true);
+		label.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		
 	}
 }
 
